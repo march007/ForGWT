5ee3c46989126842182ed193eebcab3f86d31586
march007 <yrgant@rambler.ru>
2013-06-26 15:03:21 +0400

    new function 1

---
+++ HiberGWTSpring/src/main/java/ru/vrn/com/shared/model/AccessVPN.java
@@ -0,0 +1,13 @@
+package ru.vrn.com.shared.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import ru.vrn.com.shared.model.superclass.Access;
+
+@Entity
+@Table(name = "Access_VPN")
+public class AccessVPN extends Access{
+	
+
+}
